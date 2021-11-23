a80f0487eced0f21bcd2dacf19ae440f02c92482
MatiasBravo <matiasalejandro.bravovaisman@mercadolibre.com>
2021-11-22 23:06:03 -0300

    specify violations

--- pkg/violations/violations.go
+++ pkg/violations/violations.go
@@ -1,5 +1,21 @@
 package violations
 
 const (
+	// Once created, the account should not be updated or recreated
 	AccountAlreadyInitialized = "account-already-initialized"
+
+	// No transaction should be accepted without a properly initialized account
+	AccountNotInitialized = "account-not-initialized"
+
+	// No transaction should be accepted when the card is not active
+	CardNotActive = "card-not-active"
+
+	// The transaction amount should not exceed the available limit
+	InsufficientLimit = "insufficient-limit"
+
+	// There should be no more than 3 transactions within a 2 minutes interval
+	HighFrequencySmallInterval = "high-frequency-small-interval"
+
+	// There should be no more than 1 similar transaction (same amount and merchant) within a 2 minutes interval
+	DoubledTransaction = "doubled-transaction"
 )
